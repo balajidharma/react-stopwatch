f96fd71083e49d05c99bd5393b3a7ca6d6d1bdc4
balajidharma <balajidharma@hotmail.com>
2025-01-18 11:04:41 -0500

    Updated components folder name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Stopwatch from './Component/Stopwatch';
+import Stopwatch from './components/Stopwatch';
 
 function App() {
   const [count, setCount] = useState(0)
